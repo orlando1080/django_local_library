88b70c5b8e8faf422b307c5c68d59a3a37c5b0b3
Lewis Sewell <lewislsewell@gmail.com>
2022-11-18 17:05:00 +0000

    updated CSRF_TRUSTED_ORIGINS

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ## (replace the string below with your own site URL):
 
 ALLOWED_HOSTS = ['web-production-3ad8.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-3ad8.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3ad8.up.railway.app']
 
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
